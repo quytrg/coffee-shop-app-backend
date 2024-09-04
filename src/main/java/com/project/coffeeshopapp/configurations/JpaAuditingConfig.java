4f60b12903502d51cae80fedf035a0e9aef559c6
quytrg <truongvanquy2k3@gmail.com>
2024-09-05 00:10:51 +0700

    auditing the author of ahanges with Spring Security

--- src/main/java/com/project/coffeeshopapp/configurations/JpaAuditingConfig.java
+++ src/main/java/com/project/coffeeshopapp/configurations/JpaAuditingConfig.java
@@ -1,9 +1,13 @@
 package com.project.coffeeshopapp.configurations;
 
+import com.project.coffeeshopapp.models.CustomUserDetails;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 import java.util.Optional;
 
 @Configuration
@@ -16,7 +20,20 @@ public class JpaAuditingConfig {
     public class AuditorAwareImpl implements AuditorAware<String> {
         @Override
         public Optional<String> getCurrentAuditor() {
-            return null;
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+            if (authentication == null || !authentication.isAuthenticated()) {
+                return Optional.empty();
+            }
+
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof CustomUserDetails userDetails) {
+                String fullName = userDetails.getUser().getFullName();
+                String auditorInfo = userDetails.getUsername() + " - " + (fullName != null ? fullName : "");
+                return Optional.of(auditorInfo);
+            } else {
+                return Optional.empty();
+            }
         }
     }
 }
